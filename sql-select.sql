6b42c003d8a99bbe807727782eafa8b34760cd13
Suttipong Rangseethammakul <51299265+SRangseethammakul@users.noreply.github.com>
2024-05-07 23:26:43 +0700

    Update sql-select.sql

--- sql-select.sql
+++ sql-select.sql
@@ -1,2 +1,8 @@
 #1683. Invalid Tweets
 select tweet_id from Tweets where CHAR_LENGTH(content) > 15
+
+# 1148. Article Views I
+select distinct author_id as id from Views where author_id = viewer_id order by id 
+
+# 595. Big Countries
+select name, population, area from World where area >= 3000000 or population >= 25000000
